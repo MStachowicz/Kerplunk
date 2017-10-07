ca01288efbda8280296def9d92ecbed90a470c0e
Michal S - Mac <michal_1995@hotmail.co.uk>
2017-10-07 23:54:30 +0100

    Added the Shader class constructor code which creates the fragment and vertex objects and links them to the shader program. Also added code for utility functions to set uniform bool, int and float variables.

--- Kerplunk/Shader.h
+++ Kerplunk/Shader.h
@@ -17,6 +17,16 @@ public:
 	// Reads and builds the shader
 	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
 	
+	// Activates the shader
+	void use();
+
+	// Utility uniform functions
+
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+
+
 	~Shader();
 
 	// The program ID
