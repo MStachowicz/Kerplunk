b90a2bde36b12473efcf8df6fde2e382b4376d80
Michal <michal_1995@hotmail.co.uk>
2017-10-08 14:32:31 +0100

    Added a uniform float offset based on the time, used to change the x position of the two triangles and cycle their colour.

--- Kerplunk/Main.cpp
+++ Kerplunk/Main.cpp
@@ -119,12 +119,11 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-
-		//float timeValue = glfwGetTime();
-		//float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		//int horizontalOffsetLocation = glGetUniformLocation(ourShader.ID, "horizontalOffset");
 		ourShader.use();
-		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		ourShader.setFloat("horizontalOffset", greenValue);
 
 
 		glBindVertexArray(VAO[0]);
--- Kerplunk/VertexShader.vert
+++ Kerplunk/VertexShader.vert
@@ -2,10 +2,12 @@
 layout (location = 0) in vec3 aPos;		// the position variable has attribute position 0
 layout (location = 1) in vec3 aColor;	// colour variable has attribute position 1
 
+uniform float horizontalOffset;
+
 out vec3 ourColor; // specify a color output to the fragment shader
 
 void main()
 		{
-			gl_Position = vec4(aPos, 1.0); 
-			ourColor = aColor;
+			gl_Position = vec4(aPos.x + horizontalOffset, aPos.y, aPos.z, 1.0) * vec4(-1.0, -1.0,-1.0, 1.0); 
+			ourColor = vec3(aColor.r + horizontalOffset, aColor.g - horizontalOffset ,aColor.b);
 		};
