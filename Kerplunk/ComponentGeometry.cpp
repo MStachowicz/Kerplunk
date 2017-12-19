abb5bf086b2bc522b8fa8b157109f409305025c4
Michal S - Mac <michal_1995@hotmail.co.uk>
2017-12-19 20:38:01 +0000

    Added loading constructor using a supplied vertex vector. Also added render and bufferData methods completing the geometry core functionality.

--- Kerplunk/ComponentGeometry.cpp
+++ Kerplunk/ComponentGeometry.cpp
@@ -1,11 +1,43 @@
 #include "ComponentGeometry.h"
 
+
 ComponentGeometry::ComponentGeometry(std::string filePath) : IComponent(IComponent::ComponentFlags::COMPONENT_GEOMETRY)
 {
 }
 
-ComponentGeometry::ComponentGeometry(float pVertices[]) : IComponent(IComponent::ComponentFlags::COMPONENT_GEOMETRY)
+ComponentGeometry::ComponentGeometry(std::vector<float> pVertices) : IComponent(IComponent::ComponentFlags::COMPONENT_GEOMETRY)
 {
+	vertices = pVertices;
+	numberOfTriangles = vertices.size() / 8; // 3 positions 3 normals 2 texcoords
+	BufferData();
+}
+
+void ComponentGeometry::render()
+{
+	glBindVertexArray(VAO_Handle);
+	glDrawArrays(GL_TRIANGLES, 0, numberOfTriangles);
+}
+
+void ComponentGeometry::BufferData()
+{
+	glGenVertexArrays(1, &VAO_Handle);
+	glGenBuffers(1, &VBO_Handle);
+
+	glBindVertexArray(VAO_Handle);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_Handle);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size(), &vertices, GL_STATIC_DRAW);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// texture attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//unbind vao
+	glBindVertexArray(0);
 }
 
 ComponentGeometry::~ComponentGeometry() {}
--- Kerplunk/ComponentGeometry.h
+++ Kerplunk/ComponentGeometry.h
@@ -1,20 +1,33 @@
 #ifndef COMPONENTGEOMETRY_H
 #define COMPONENTGEOMETRY_H
 
-#include "IComponent.h"
+#include <vector>
 #include <string>
 #include <glm/glm.hpp>
+#include <GL/glew.h>
+//#include <GLFW/glfw3.h>
+#include "IComponent.h"
 
 class ComponentGeometry : public IComponent
 {
 public:
-	glm::vec3 position;
+	// Stores all the vertex data associated with the object. Order = Position, Normal, Texture coord
+	std::vector<float> vertices;
+	unsigned int numberOfTriangles;
+	unsigned int VAO_Handle;
+	unsigned int VBO_Handle;
 
 	// Create a geometry oject loading in vertex data from a file.
 	ComponentGeometry(std::string filePath);
 	// Create a geometry object using an array of vertex data.
-	ComponentGeometry(float pVertices[]);
+	ComponentGeometry(std::vector<float> pVertices);
+
+	void render();
 
 	~ComponentGeometry();
+
+private:
+	void BufferData();
+
 };
 #endif
