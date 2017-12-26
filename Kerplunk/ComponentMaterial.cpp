56c5b617d4ca77b34b4a1e8c82d0f0e75adf6671
Michal S - Mac <michal_1995@hotmail.co.uk>
2017-12-26 16:51:56 +0000

    Added the material component used to render entities without using textures.

---
+++ Kerplunk/ComponentMaterial.cpp
@@ -0,0 +1,26 @@
+#include "ComponentMaterial.h"
+
+//ComponentMaterial::ComponentMaterial() : IComponent(IComponent::ComponentFlags::COMPONENT_MATERIAL)
+
+
+ComponentMaterial::ComponentMaterial()
+{
+}
+
+ComponentMaterial::ComponentMaterial(glm::vec3 pAmbientDiffuse)
+{
+}
+
+ComponentMaterial::ComponentMaterial(glm::vec3 pAmbientDiffuse, glm::vec3 pSpecular)
+{
+}
+
+ComponentMaterial::ComponentMaterial(glm::vec3 pAmbientDiffuse, glm::vec3 pSpecular, float pShininess)
+{
+}
+
+ComponentMaterial::ComponentMaterial(glm::vec3 pAmbient, glm::vec3 pDiffuse, glm::vec3 pSpecular, float pShininess)
+{
+}
+
+ComponentMaterial::~ComponentMaterial() {}
