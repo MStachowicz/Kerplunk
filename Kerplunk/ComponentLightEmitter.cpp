c2f84cd32b878e95167ef1053f2d1c7e68d79734
Michal S - Mac <michal_1995@hotmail.co.uk>
2018-01-02 13:48:50 +0000

    Corrected misspelled intialization list members.

--- Kerplunk/ComponentLightEmitter.cpp
+++ Kerplunk/ComponentLightEmitter.cpp
@@ -1,7 +1,7 @@
 #include "ComponentLightEmitter.h"
 
 ComponentLightEmitter::ComponentLightEmitter(glm::vec3 pAmbient, glm::vec3 pDiffuse, glm::vec3 pSpecular) : IComponent(ComponentFlags::COMPONENT_LIGHT_EMITTER),
-Ambient(pAmbient), Diffuse(pDiffuse), Specular(pSpecular)
+ambient(pAmbient), diffuse(pDiffuse), specular(pSpecular)
 {
 }
 
