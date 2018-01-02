42f8db9868232c057ee08d87a7cb1330adc27d03
Michal S - Mac <michal_1995@hotmail.co.uk>
2018-01-02 03:24:02 +0000

    Added the light properties as members of the component and completed the constructor.

--- Kerplunk/ComponentLightEmitter.cpp
+++ Kerplunk/ComponentLightEmitter.cpp
@@ -1,8 +1,7 @@
 #include "ComponentLightEmitter.h"
 
-
-
-ComponentLightEmitter::ComponentLightEmitter() : IComponent(ComponentFlags::COMPONENT_LIGHT_EMITTER)
+ComponentLightEmitter::ComponentLightEmitter(glm::vec3 pAmbient, glm::vec3 pDiffuse, glm::vec3 pSpecular) : IComponent(ComponentFlags::COMPONENT_LIGHT_EMITTER),
+Ambient(pAmbient), Diffuse(pDiffuse), Specular(pSpecular)
 {
 }
 
--- Kerplunk/ComponentLightEmitter.h
+++ Kerplunk/ComponentLightEmitter.h
@@ -2,11 +2,16 @@
 #define COMPONENTLIGHTEMITTER_H
 
 #include "IComponent.h"
+#include <glm\glm.hpp>
 
 class ComponentLightEmitter : public IComponent
 {
 public:
-	ComponentLightEmitter();
+	glm::vec3 Ambient;
+	glm::vec3 Diffuse;
+	glm::vec3 Specular;
+
+	ComponentLightEmitter(glm::vec3 pAmbient, glm::vec3 pDiffuse, glm::vec3 pSpecular);
 	~ComponentLightEmitter();
 };
 #endif
