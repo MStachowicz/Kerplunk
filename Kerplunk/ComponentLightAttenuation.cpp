c89e82b12826ab32bf82f0e48a48467bc605616f
Michal S - Mac <michal_1995@hotmail.co.uk>
2018-01-02 12:25:35 +0000

    Added the missing initialization list for member variables.

--- Kerplunk/ComponentLightAttenuation.cpp
+++ Kerplunk/ComponentLightAttenuation.cpp
@@ -1,6 +1,7 @@
 #include "ComponentLightAttenuation.h"
 
-ComponentLightAttenuation::ComponentLightAttenuation(float pConstant, float pLinear, float pQuadratic) : IComponent(IComponent::ComponentFlags::COMPONENT_LIGHT_ATTENUATION)
+ComponentLightAttenuation::ComponentLightAttenuation(float pConstant, float pLinear, float pQuadratic) : IComponent(IComponent::ComponentFlags::COMPONENT_LIGHT_ATTENUATION),
+constant(pConstant), linear(pLinear), quadratic(pQuadratic)
 {
 }
 
