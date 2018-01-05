a6614518669e23ba85d23c16e705c08659bfa588
Michal S - Mac <michal_1995@hotmail.co.uk>
2018-01-05 12:06:59 +0000

    Added the directional shadow emitter component. Sets up a framebuffer object which is filled at render time with depth values.

--- Kerplunk/ComponentDirectionShadowEmitter.cpp
+++ Kerplunk/ComponentDirectionShadowEmitter.cpp
@@ -4,8 +4,38 @@ ComponentDirectionShadowEmitter::ComponentDirectionShadowEmitter(float pNear, fl
 near(pNear), far(pFar), size(pSize)
 {
 	lightProjection = glm::ortho(-size, size, -size, size, near, far);
+	setupFBO();
 }
 
+
+void ComponentDirectionShadowEmitter::setupFBO()
+{
+	// Generating the framebuffer object
+	glGenFramebuffers(1, &FBO_Handle);
+
+	// Creating a 2D texture to be used as the frame buffers depth buffer
+	glGenTextures(1, &depthTexture);
+	glBindTexture(GL_TEXTURE_2D, depthTexture);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, RESOLUTIONX, RESOLUTIONY, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	// texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+	float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // setting border to prevent shadow in samples outside the light perspective
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+	// Attaching the depth texture to the framebuffers depth buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO_Handle);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTexture, 0);
+	// No need to attach a colour buffer so setting draw and read buffer to GL_NONE to signify this.
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
 
 ComponentDirectionShadowEmitter::~ComponentDirectionShadowEmitter()
 {
--- Kerplunk/ComponentDirectionShadowEmitter.h
+++ Kerplunk/ComponentDirectionShadowEmitter.h
@@ -2,6 +2,7 @@
 #include "IComponent.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <GL/glew.h>
 
 class ComponentDirectionShadowEmitter : public IComponent
 {
@@ -10,9 +11,17 @@ public:
 	float far;
 	float size;
 
+	unsigned int FBO_Handle;
+	unsigned int depthTexture;
+	const unsigned int RESOLUTIONX = 1024;
+	const unsigned int RESOLUTIONY = 1024;
+
 	glm::mat4 lightProjection;
 
 	ComponentDirectionShadowEmitter(float pNear, float pFar, float pSize);
 	~ComponentDirectionShadowEmitter();
+
+private:
+	void setupFBO(); // Generates the FBO and texture depth attachment of the FBO.
 };
 
